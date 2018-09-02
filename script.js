be3dae3ab9125bb56a61046fafeeac8cc0dc5ddc
Maryadi <maryadipoipo187@gmail.com>
2018-09-02 23:59:18 +0800

    add hover red using js

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ var sidemenu = document.getElementById("mysidemenuid");
 button.addEventListener("click", function () {
     sidemenu.style.width = "110px";
 })
+button.addEventListener("mousemove", function () {
+    var bars = document.querySelectorAll(".bar1");
+    bars.forEach(bar => {
+        bar.style.width = "30px";
+        bar.style.backgroundColor = "red";
+    });
+})
+
+button.addEventListener("mouseleave", function () {
+    var bars = document.querySelectorAll(".bar1");
+    bars.forEach(bar => {
+        bar.style.width = "10px";        
+        bar.style.backgroundColor = "black";
+    });
+})
 
 var menuitem = document.querySelector("#mysidemenuid a");
 menuitem.addEventListener("click", function () {
